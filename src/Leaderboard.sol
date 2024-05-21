befa1764f60def24c62e1bd33745df3fbc8b6af1
JonathanCheng123 <jcmohr9@gmail.com>
2024-05-20 19:33:34 -0700

    implemented initial buyer and seller

--- src/Leaderboard.sol
+++ src/Leaderboard.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import "./Leaderboard.sol";
 
-contract RentManager {
+contract Leaderboard {
     //item struct
     struct Item {
         uint256 itemId;
         string itemName;
         uint256 itemPrice;
-        uint256 prevSoldPrice;
+        uint256 itemDiscount;
         address owner;
         bool forsale;
     }
@@ -49,17 +48,16 @@ contract RentManager {
     // the item id can be used as index to access the 'items' array
     mapping(uint256 => Item) private id2item;
 
-    Leaderboard leaderboard;
-    constructor(Leaderboard lboard) {
+
+    constructor() {
         roles[msg.sender] = 1;
-        leaderboard = lboard;
         // create dummy invoice for default invoice to connect to
         currItemId = 0;
         Item memory defaultItem = Item({
             itemId: 0,
             itemName: "default",
             itemPrice: 0,
-            prevSoldPrice: 0,
+            itemDiscount: 0,
             owner: address(0),
             forsale: false
         });
@@ -110,17 +108,19 @@ contract RentManager {
                 return false;
             }
 
-            int discount; // used for leaderboard only
+            uint256 currDiscount;
             if(itemPrice >= id2item[itemId].itemPrice) {
-                discount = int(id2item[itemId].itemPrice) - int(itemPrice);
+                currDiscount = 0;
             } else {
-                discount += int(id2item[itemId].itemPrice) - int(itemPrice);
+                currDiscount += id2item[itemId].itemPrice - itemPrice;
             }
+
+
 
             Item memory item = Item({
                 itemId: itemId,
                 itemName: itemName,
-                prevSoldPrice: id2item[itemId].prevSoldPrice,
+                itemDiscount: currDiscount,
                 itemPrice: itemPrice,
                 owner: address(msg.sender),
                 forsale: forsale
@@ -133,8 +133,8 @@ contract RentManager {
             Item memory item = Item({
                 itemId: currItemId,
                 itemName: itemName,
+                itemDiscount: 0,
                 itemPrice: itemPrice,
-                prevSoldPrice: 0,
                 owner: address(msg.sender),
                 forsale: forsale
             });
@@ -148,15 +148,15 @@ contract RentManager {
     }
 
     //view items that are being sold
-    function viewItems() public returns (Item[] memory) {
-        Item[] storage forsaleItems;
-        for(uint256 i = 0; i < items.length; i++) {
-            if (items[i].forsale){
-                forsaleItems.push(items[i]);
-            }
-        }
-        return forsaleItems;
-    }
+    // function viewItems() public returns (Item[] memory) {
+    //     Item[] storage forsaleItems;
+    //     for(uint256 i = 0; i < items.length; i++) {
+    //         if (items[i].forsale){
+    //             forsaleItems.push(items[i]);
+    //         }
+    //     }
+    //     return forsaleItems;
+    // }
 
     //buy an item
     function purchaseItem(uint256 itemId) public returns (bool) {
@@ -178,15 +178,17 @@ contract RentManager {
         balances[invoices[addr2invoice[msg.sender]].item.owner] += item.itemPrice;
         // change the owner of the item
         item.owner = invoices[addr2invoice[msg.sender]].buyer;
-        item.prevSoldPrice = item.itemPrice;
-        // when we want to update the leaderboard, do something here
     }
     
     
     //TODO
     function viewLeaderBoard() public returns (string[] memory) {
-        
+
     }
+
+    
+
+
 
     // Check the balance of the caller 
     // Rets:
@@ -216,7 +218,7 @@ contract RentManager {
     // Specs:
     //   - only unregistered users can register; otherwise, return false
     //   - a user can only register once; otherwise, return false
-    function registerHost() public returns (bool) {
+    function register() public returns (bool) {
         // ============================
         // add your implementation here
         // ============================
@@ -228,5 +230,116 @@ contract RentManager {
         }
         return false;
     }
+
+    // Send out an invoice to a guest
+    // Args:
+    //   - toAddr (address): guest address that you want to send the invoice to
+    //   - amount (uint256): amount requested
+    // Rets:
+    //   - (bool): whether the operation is successful
+    // Specs:
+    //   - only host can send an invoice; otherwise, return false
+    //   - an invoice can only be sent to a guest; otherwise, return false
+    //   - the host cannot send an invoice if the host has an existing invoice that 
+    //     has not been fully paid out yet; in this case, return false
+    // function sendInvoice(address toAddr, uint256 amount) public returns (bool) {
+    //     // ============================
+    //     // add your implementation here
+    //     // ============================
+    //     if(roles[msg.sender] == 2) {
+    //         if(roles[toAddr] == 3) {
+    //             if(invoices[addr2invoice[msg.sender]].remainingAmount == 0) {
+    //                 addr2invoice[toAddr] = invoices.length;
+    //                 addr2invoice[msg.sender] = invoices.length;
+
+    //                 Invoice memory invoice = Invoice({
+    //                     amount: amount,
+    //                     remainingAmount: amount,
+    //                     host: msg.sender,
+    //                     guest: toAddr
+    //                 });
+                    
+    //                 invoices.push(invoice);
+    //                 return true;
+    //             }
+                
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // View the latest invoice
+    // Rets:
+    //   - (uint256, uint256, address, address): a 4-tuple indicating total amount requested,
+    //     remaining amount that the guest needs to pay, host that sends the invoice, and guest
+    //     that receives the invoice
+    // Specs:
+    //   - only registered users can view invoice; otherwise return (0, 0, address(0), address(0))
+    //   - if (for the guest) no invoice has been received before or (for the host) no invoice has
+    //     been sent out before, simply return (0, 0, address(0), address(0))
+    // function viewInvoice() public view returns (uint256, uint256, address, address) {
+    //     // ============================
+    //     // add your implementation here
+    //     // ============================
+    //     if(roles[msg.sender] > 0) {
+    //         //host
+    //         if(addr2invoice[msg.sender] == 0) {
+    //             return (0, 0, address(0), address(0));
+    //         }
+    //         return (invoices[invoices.length - 1].amount, invoices[invoices.length - 1].remainingAmount,
+    //         invoices[invoices.length - 1].host, invoices[invoices.length - 1].guest);
+    //     }
+    //     return (0, 0, address(0), address(0));
+    // }
+
+    // // Pay the invoice
+    // // Args:
+    // //   - amount (uint256): the amount that the guest wants to pay
+    // // Rets:
+    // //   - (bool): whether the operation is successful
+    // // Specs:
+    // //   - only guest can pay invoice; otherwise return false
+    // //   - if the amount to pay is more than the guest's balance, return false
+
+    // //   - if the amount to pay is less than the remaining amout of the invoice,
+    // //     pay the amount to the corresponding host and update the remaining amount
+    // //     on the invoice
+
+    // //   - if the amount to pay is more than the remaining amount of the invoice,
+    // //     pay the remaining amout the the corresponding host and update the invoice
+    // //     to "paid out" by setting the remaining amount to 0
+
+    // //   - don't forget to deduce the actually paid amount from the guest's balance
+    // function payInvoice(uint256 amount) public returns (bool) {
+    //     // ============================
+    //     // add your implementation here
+    //     // ============================
+    //     if(roles[msg.sender] == 3) {
+    //         if(amount < balances[msg.sender]) {
+    //             uint256 remaining = invoices[addr2invoice[msg.sender]].remainingAmount;
+
+    //             if(amount < remaining) {
+    //                 balances[invoices[addr2invoice[msg.sender]].host] += amount;
+    //                 balances[msg.sender] -= amount;
+
+    //                 invoices[addr2invoice[msg.sender]].remainingAmount -= amount;
+    //                 //should be equal to balances[invoices[addr2invoice[msg.sender]].guest]
+    //             }
+    //             else {
+    //                 balances[invoices[addr2invoice[msg.sender]].host] += remaining;
+    //                 balances[msg.sender] -= remaining;
+
+    //                 invoices[addr2invoice[msg.sender]].remainingAmount = 0;
+
+
+    //             }
+    //             return true;
+
+                
+    //         }
+    //     }
+    //     return false;
+    // }
 
 }
